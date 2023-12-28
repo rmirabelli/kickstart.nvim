7cf185c1384a7d2b18b1dff6a706d4966c82132f
Russell Mirabelli <russell.mirabelli@bottlerocketstudios.com>
2023-12-28 08:54:06 -0600

    title

--- init.lua
+++ init.lua
@@ -326,6 +326,10 @@ vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnos
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
 
+-- Title
+vim.o.title = true
+vim.o.titlestring = "nvim: %t"
+
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
