7c05bf6d7a459e45303a664856d3c67e70665a24
Russell Mirabelli <russell.mirabelli@bottlerocketstudios.com>
2024-04-29 14:25:08 -0500

    block cursor

--- init.lua
+++ init.lua
@@ -317,6 +317,8 @@ vim.o.completeopt = 'menuone,noselect'
 -- NOTE: You should make sure your terminal supports this
 vim.o.termguicolors = true
 
+vim.opt.guicursor = 'i:block'
+
 -- [[ Basic Keymaps ]]
 
 -- Keymaps for better default experience
